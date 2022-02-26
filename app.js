55d0d7aa17ba558b83c1559350b90ed0d252c22e
manutdmohit <saudmohit@gmail.com>
2022-02-26 11:17:05 +0545

    cloudinary setup

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const app = express();
 
 const fileUpload = require('express-fileupload');
 
+// USe V2
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 // Database
 const connectDB = require('./db/connect');
 
