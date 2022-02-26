fa607764017a3f8c57df1724971f55ea5cfad22a
manutdmohit <saudmohit@gmail.com>
2022-02-26 10:58:55 +0545

    Check file exists, check file type, check size

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,11 +1,30 @@
 const path = require('path');
-
-const Product = require('../models/Product');
-
 const { StatusCodes } = require('http-status-codes');
 
+const Product = require('../models/Product');
+const CustomError = require('../errors');
+
 exports.uploadProductImage = async (req, res) => {
+  // check if file exists
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded');
+  }
+
   const productImage = req.files.image;
+
+  // check format
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image Only');
+  }
+
+  const maxSize = 1024 * 1024;
+
+  // check image size
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      'Please Upload Image Smaller than 1MB Only'
+    );
+  }
 
   const imagePath = path.join(
     __dirname,
