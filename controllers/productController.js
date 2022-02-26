335ebf4902e9dd05f774b013bbbe0ee0d8cd6de8
manutdmohit <saudmohit@gmail.com>
2022-02-26 09:59:12 +0545

    create product fail

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,9 @@ const Product = require('../models/Product');
 const { StatusCodes } = require('http-status-codes');
 
 exports.createProduct = async (req, res) => {
-  res.send('Create Product');
+  const product = await Product.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ product });
 };
 
 exports.getAllProducts = async (req, res) => {
